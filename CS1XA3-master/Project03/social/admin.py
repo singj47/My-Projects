7d1c9c0ac9b3d8433f088d41c7c4ffe4019c7183
singj47 <singj47@mcmaster.ca>
2021-11-17 17:43:04 -0500

    editing

--- CS1XA3-master/Project03/social/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Interest,UserInfo,FriendRequest,Post
-# Register your models here.
-admin.site.register(Interest)
-admin.site.register(UserInfo)
-admin.site.register(FriendRequest)
-admin.site.register(Post)
